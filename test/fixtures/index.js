4a31e8979533dfccd99157832def834f134f340a
Rudi Giesler <rudi@praekelt.com>
2015-05-27 17:22:11 +0200

    Revert "Add fixture for getting an xform from an external server"
    This reverts commit 8ef11ed11f09b9f4ce7d53234fdb6100c7fe8451.

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,17 +1,3 @@
-var sample_xform = require('./sample_xform');
-
 module.exports = function() {
-    return [
-        {
-            request: {
-                method: "GET",
-                url: "http://www.example.org/xform00",
-                params: {},
-            },
-            response: {
-                code: "200",
-                data: sample_xform,
-            },
-        },
-    ];
+    return [];
 };
