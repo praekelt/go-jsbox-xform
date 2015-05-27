8ef11ed11f09b9f4ce7d53234fdb6100c7fe8451
Rudi Giesler <rudi@praekelt.com>
2015-05-27 11:38:17 +0200

    Add fixture for getting an xform from an external server

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,3 +1,17 @@
+var sample_xform = require('./sample_xform');
+
 module.exports = function() {
-    return [];
+    return [
+        {
+            request: {
+                method: "GET",
+                url: "http://www.example.org/xform00",
+                params: {},
+            },
+            response: {
+                code: "200",
+                data: sample_xform,
+            },
+        },
+    ];
 };
